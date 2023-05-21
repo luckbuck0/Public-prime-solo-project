d4e820b9c661c35d25110780386b8f1c3e96aca9
Luckie Bah <bah966@gmail.com>
2023-05-22 00:23:31 +0200

    DATABASE
    -Created the necessary tables on postico
    
    -Copied the sql query into the database.sql file on vs code
    
    -Next step is to test the databases and make sure the connection
    between server and database is established and data is being retrieved

---
+++ Steps.md
@@ -0,0 +1,8 @@
+
+//--------------------STEPS---------------------------------
+
+//1. Write the necessary sql in postico then copy and past
+ file into database.sql
+
+ //2. Test the database make sure they work insert some props
+ ie users bookmarks etc.
--- database.sql
+++ database.sql
@@ -3,8 +3,30 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+CREATE TABLE "img" (
+    "id" SERIAL PRIMARY KEY,
+    "photo_url" VARCHAR (1000) 
+);
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (1000) NOT NULL,
+    "image_id" INT REFERENCES  "img"
+);
+
+CREATE TABLE "workspaces" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES  "user",
+    "name" VARCHAR (80),
+    "Category" VARCHAR (80)
+);
+
+CREATE TABLE "bookmark" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) NOT NULL,
+    "url" VARCHAR (1000) NOT NULL,
+    "photo" VARCHAR (1000) NOT NULL,
+    "workspace_id"INT REFERENCES  "workspaces"
 );
